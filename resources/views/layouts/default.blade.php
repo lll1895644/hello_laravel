5db9babf08a2fa2a8987d191e6c597d608fb636f
lll1895644 <1029236462@qq.com>
2018-05-11 09:10:48 +0000

    Finish login and loginout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,9 +7,12 @@
   <body>
 	@include('layouts._header')
     <div class="container">
+      <div class="col-md-offset-1 col-md-10">
       @include('shared._message')
       @yield('content')
       @include('layouts._footer')
-    </div>    
+      </div>
+    </div> 
+    <script src="/js/app.js"></script>  
   </body>
 </html>
