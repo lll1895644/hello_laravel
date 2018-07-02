75d7158e739e7cc920faaf2a030429b2eb0ae066
lll1895644 <1029236462@qq.com>
2018-07-02 10:05:54 +0000

    Finish user edit,update,index,and destroy

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -8,6 +8,14 @@ use Auth;
 
 class SessionController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+
     public function create()
     {
     	return view('session.create');
@@ -15,13 +23,15 @@ class SessionController extends Controller
 
     public function store(Request $request)
     {
+
     	$credetail = $this->validate($request,[
     		'email'=>'required|email|max:255',
     		'password'=>'required'
     	]);
+
     	if(Auth::attempt($credetail,$request->has('remember'))){
     		session()->flash('success','欢迎回来！');
-    		return redirect()->route('users.show',[Auth::user()]);
+    		return redirect()->intended(route('users.show',[Auth::user()]));
     	}else{
     		session()->flash('danger','很抱歉，你的邮箱或者密码不正确！');
     		return redirect()->back();
@@ -29,9 +39,9 @@ class SessionController extends Controller
     	return;
     }
 
-    public funtion destroy()
+    public function destroy()
     {
-    	Auth::loginout();
+    	Auth::logout();
     	session()->flash('success','退出成功');
     	return redirect('login');
     }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Model\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->makevisible(['password','remember_token'])->toArray());
+        $user = User::find(1);
+        $user->name = 'winter';
+        $user->email = '3541700767@qq.com';
+        $user->password = bcrypt('password');
+        $user->is_admin = true;
+        $user->save(); 
+    }
+}
